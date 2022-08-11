651d7bb4741182bbea36f9edfb81e1d710924b5b
justsaykk <kaikein.woo@gmail.com>
2022-08-11 14:43:21 +0800

    Trying to use a forwarder

--- src/main/java/restfulapi/cryptocompare/controllers/apiController.java
+++ src/main/java/restfulapi/cryptocompare/controllers/apiController.java
@@ -6,8 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -51,5 +54,12 @@ public class apiController {
             @RequestParam(name = "symbol") String symbol) {
         String responseObj = apiSvc.getSignal(symbol, app).toString();
         return new ResponseEntity<String>(responseObj, HttpStatus.OK);
+    }
+
+    @PostMapping("/symbol")
+    public String forwarder(@RequestBody MultiValueMap<String, String> form) {
+        String symbol = form.getFirst("symbol");
+        String fiat = form.getFirst("fiat");
+        return "redirect:/api/%s/%s".formatted(symbol, fiat);
     }
 }
